035f0e7d1ededac2158662ef1a3d6cd1244212b0
admin <admin@MacBook-Pro-cua-admin.local>
2021-02-19 07:58:55 +0700

    Chỉnh style

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,9 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+    const app = await NestFactory.create(AppModule);
+    app.enableCors();
+    app.useWebSocketAdapter(new WsAdapter(app));
+    await app.listen(3000);
 }
 bootstrap();
